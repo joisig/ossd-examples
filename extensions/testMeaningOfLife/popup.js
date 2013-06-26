750bc4ed3f08452b0757f20bcb5ccf119205f551
Joi Sigurdsson <joi@chromium.org>
2013-06-26 15:32:08 +0000

    Add a simple test extension for the meaningOfLife API I added to Chrome.

---
+++ extensions/testMeaningOfLife/popup.js
@@ -0,0 +1,3 @@
+chrome.meaningOfLife.whatIsIt('the ultimate question of life, the universe and everything', function(answer) {
+    alert('The answer to ' + answer.original_question + ' is: ' + answer.meaning_of_life);
+});
